b8cefdefec34767b6f51546188e4c456a2e17d7a
entbappy <entbappy73@gmail.com>
2022-01-25 21:50:01 +0600

    updated

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,8 +1,17 @@
 import yaml 
 import logging
+import os
 
-def read_yaml(path_to_yaml: str) -> dict:
+
+def read_yaml(self,path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
     logging.info(f"yaml file: {path_to_yaml} loaded successfully")
     return content
+
+
+def user_info(clint):
+    response = clint.get("/me")
+    return response.json()
+   
+
