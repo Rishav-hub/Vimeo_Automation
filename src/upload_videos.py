7d04fd631f9ba8d307feb997a3ab4efb5514f06e
entbappy <entbappy73@gmail.com>
2022-01-25 18:28:48 +0600

    exception added

--- src/upload_videos.py
+++ src/upload_videos.py
@@ -27,31 +27,35 @@ def video_upload(secret_path, config_path):
     current_path = os.getcwd()
 
     uploader_path = os.path.join(current_path, video_path)
+    try:
+        client = vimeo.VimeoClient(token=tokens,key=keys, secret=sec)
+        response = client.get("/me")
+        # print(response.json())
 
-    client = vimeo.VimeoClient(token=tokens,key=keys, secret=sec)
-    response = client.get("/me")
-    # print(response.json())
+        #uploading videos
+        name_list = os.listdir(uploader_path)
+        url_list = []
 
-    #uploading videos
-    name_list = os.listdir(uploader_path)
-    url_list = []
+        for name in glob(uploader_path+"*.mp4"):
+            url_list.append(name)
+        
+        # print(len(name_list))
+        # print(url_list)
 
-    for name in glob(uploader_path+"*.mp4"):
-        url_list.append(name)
+        for indx in range(len(name_list)):
+            video_name = name_list[indx]
+            video_url = url_list[indx]
+
+            uri = client.upload(video_url, data={
+            'name': video_name,
+            'description': 'The description goes here.',
+            "privacy": { "view": "nobody"}
+            })
+
+            logging.info('Your video URI is: %s' % (uri))
     
-    # print(len(name_list))
-    # print(url_list)
-
-    for indx in range(len(name_list)):
-        video_name = name_list[indx]
-        video_url = url_list[indx]
-
-        uri = client.upload(video_url, data={
-        'name': video_name,
-        'description': 'The description goes here.',
-        "privacy": { "view": "nobody"}
-        })
-
-        print('Your video URI is: %s' % (uri))
+    except Exception as e:
+        logging.error(e)
+        print(e)
 
 # video_upload("secrets/secret.yaml", "config/config.yaml")
