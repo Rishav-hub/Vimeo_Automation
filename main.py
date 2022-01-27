7f6538de9e187ec909aeaf178be05043cc7ff7fd
Rishav-hub <9930046@gmail.com>
2022-01-27 15:33:25 +0530

    Fixed Final Bug- "Code running properly"

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # from src.upload_videos import VimeoEmbed
+from gevent import config
 from src.embed_link import VimeoEmbed
-
+from src.utils.all_utils import read_yaml
 
 # video_upload("secrets/secret.yaml", "config/config.yaml")
 
 embed_obj=VimeoEmbed("secrets/secret.yaml", "config/config.yaml")
 
-embed_obj.get_embed("big_data")
+config_path = "config/config.yaml"
+config_data = read_yaml(config_path)
+
+params_path = config_data['params_path']
+
+USER_FOLDER_NAME = read_yaml(params_path)['USER_FOLDER_NAME']
+
+embed_obj.get_embed(USER_FOLDER_NAME)
--- src/embed_link.py
+++ src/embed_link.py
@@ -1,4 +1,5 @@
 from src.utils.all_utils import read_yaml, video_info, user_info, folder_info
+from src.utils.embed_utils import child_folder_list, folder_name_with_ID, parent_folder_ID_func
 import logging
 import os
 import vimeo
@@ -52,29 +53,47 @@ class VimeoEmbed:
             logging.error(e)
             raise(e)
     
-    def get_embed(self, folder_name):
-        b = self.check_folder(folder_name.lower())
+    def get_embed(self, USER_FOLDER_NAME):
+        b = self.check_folder(USER_FOLDER_NAME.lower())
         embedded_link_list = []
         video_name = []
+
+        root_folder_name = []
+
+        sub_folder_name = []
+
         try:
             if b:
                 video_json_data = video_info(self.client)
+                folder_json_data = folder_info(self.client)
+
                 data_list = video_json_data["data"]
 
                 for i in data_list:
                     if i['parent_folder']:
-                        parent_folder_name = i['parent_folder']['name']
-                        if parent_folder_name.lower() == folder_name:
-                            iframe = i['embed']['html']
-                            embedded_link_list.append(iframe.split(' ')[1][5:-2])
-                            video_name.append(i['name'])
+                        # print(i['parent_folder']['name'])
+                        iframe = i['embed']['html']
+                        embedded_link_list.append(iframe.split(' ')[1][5:-2])
+                        video_name.append(i['name'])
+                        if i['parent_folder']['name'] in child_folder_list(folder_json_data):
+                            folder_name_ID = folder_name_with_ID(folder_json_data)
+                            parent_folder_ID = parent_folder_ID_func(folder_json_data)
+                            name = folder_name_ID[i['parent_folder']['uri'].split('/')[-1]]
+                            sub_folder_name.append(name)
+                            root_folder_name.append(folder_name_ID[parent_folder_ID[i['parent_folder']['uri'].split('/')[-1]]])
+                        else:
+                            name = folder_name_ID[i['parent_folder']['uri'].split('/')[-1]]
+                            root_folder_name.append(name)
+                            sub_folder_name.append('None')
 
-                data = {'Video_Name': video_name, 'Link': embedded_link_list}
+                data = {'Video_Name': video_name, 'Link': embedded_link_list, 'ROOT_Folder_Name' :root_folder_name,'SUB_Folder_Name' : sub_folder_name}
                 df = pd.DataFrame(data)
+                df = df.where(df['ROOT_Folder_Name'] == USER_FOLDER_NAME).dropna()
 
-                excel_path = os.path.join(self.config['artifacts_path'], f"{folder_name}.xlsx")
+
+                excel_path = os.path.join(self.config['artifacts_path'], f"{USER_FOLDER_NAME}.xlsx")
                 df.to_excel(excel_path, index=False)
-                logging.info(f"{folder_name} folder embed links saved to {excel_path}")
+                logging.info(f"{USER_FOLDER_NAME} folder embed links saved to {excel_path}")
         except Exception as e:
             logging.error(e)
             raise(e)
